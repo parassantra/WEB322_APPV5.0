9ba9882333bf1105c57a7822a935a8745d065b69
Xiaochen Wang <ryan.wang23@hotmail.com>
2017-07-08 17:08:50 -0400

    change sth

--- data-service-comments.js
+++ data-service-comments.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
-mongoose.connect("mongodb://<xwang345>:<Xlxc101302#>@ds151752.mlab.com:51752/web322_a6");
+mongoose.connect("mongodb://<>:<>@ds151752.mlab.com:51752/web322_a6");
 
 var contentSchema = new Schema({
     "authorName": String,
