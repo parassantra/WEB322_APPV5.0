ddc64701bf7856c721ceb92371b72c4a242b8eba
Xiaochen Wang <ryan.wang23@hotmail.com>
2017-07-09 23:58:06 -0400

    remove README1

--- server.js
+++ server.js
@@ -63,154 +63,154 @@ app.get("/", (req, res) => {
 });
 
 // setup another route to listen on /about
-app.get("/about", (req, res) => {
-    res.render("about");
-});
+// app.get("/about", (req, res) => {
+//     res.render("about");
+// });
 
-app.get("/employees", (req, res) => {
-    if (req.query.status) {
-        data_service.getEmployeesByStatus(req.query.status).then((data) => {
-            res.render("employeeList", { data: data, title: "Employees" });
-        }).catch((err) => {
-            res.render("employeeList", { data: {}, title: "Employees" });
-        });
-    } else if (req.query.department) {
-        data_service.getEmployeesByDepartment(req.query.department).then((data) => {
-            res.render("employeeList", { data: data, title: "Employees" });
-        }).catch((err) => {
-            res.render("employeeList", { data: {}, title: "Employees" });
-        });
-    } else if (req.query.manager) {
-        data_service.getEmployeesByManager(req.query.manager).then((data) => {
-            res.render("employeeList", { data: data, title: "Employees" });
-        }).catch((err) => {
-            res.render("employeeList", { data: {}, title: "Employees" });
-        });
-    } else {
-        data_service.getAllEmployees().then((data) => {
-            res.render("employeeList", { data: data, title: "Employees" });
-        }).catch((err) => {
-            res.render("employeeList", { data: {}, title: "Employees" });
-        });
-    }
-});
+// app.get("/employees", (req, res) => {
+//     if (req.query.status) {
+//         data_service.getEmployeesByStatus(req.query.status).then((data) => {
+//             res.render("employeeList", { data: data, title: "Employees" });
+//         }).catch((err) => {
+//             res.render("employeeList", { data: {}, title: "Employees" });
+//         });
+//     } else if (req.query.department) {
+//         data_service.getEmployeesByDepartment(req.query.department).then((data) => {
+//             res.render("employeeList", { data: data, title: "Employees" });
+//         }).catch((err) => {
+//             res.render("employeeList", { data: {}, title: "Employees" });
+//         });
+//     } else if (req.query.manager) {
+//         data_service.getEmployeesByManager(req.query.manager).then((data) => {
+//             res.render("employeeList", { data: data, title: "Employees" });
+//         }).catch((err) => {
+//             res.render("employeeList", { data: {}, title: "Employees" });
+//         });
+//     } else {
+//         data_service.getAllEmployees().then((data) => {
+//             res.render("employeeList", { data: data, title: "Employees" });
+//         }).catch((err) => {
+//             res.render("employeeList", { data: {}, title: "Employees" });
+//         });
+//     }
+// });
 
-app.get("/employee/:empNum", (req, res) => {
-    // initialize an empty object to store the values
-    let viewData = {};
-    data_service.getEmployeeByNum(req.params.empNum).then((data) => {
-        viewData.data = data; //store employee data in the "viewData" object as "data"
-    }).catch(() => {
-        viewData.data = null; // set employee to null if there was an error
-    }).then(data_service.getDepartments).then((data) => {
-        viewData.departments = data; // store department data in the "viewData" object as "departments"
-                                     // loop through viewData.departments and once we have found the departmentId that matches
-                                     // the employee's "department" value, add a "selected" property to the matching
-                                     // viewData.departments object
-        for (let i = 0; i < viewData.departments.length; i++) {
-            if (viewData.departments[i].departmentId == viewData.data[0].department) {
-                viewData.departments[i].selected = true;
-            }
-        }
-        // if not add department set Selected to false and promto a message to user, message like "Please Choose Department" in html.
-        if (viewData.departments[viewData.departments.length-1].departmentId != viewData.data[0].department) {
-            viewData.departments.Selected = false;
-        }
-    }).catch(() => {
-        viewData.departments = []; // set departments to empty if there was an error
-    }).then(() => {
-        if (viewData.data == null){ // if no employee - return an error
-            res.status(404).send("Employee Not Found!!!");
-        } else {
-            res.render("employee", { viewData: viewData }); // render the "employee" view
-        }
-    });
-});
+// app.get("/employee/:empNum", (req, res) => {
+//     // initialize an empty object to store the values
+//     let viewData = {};
+//     data_service.getEmployeeByNum(req.params.empNum).then((data) => {
+//         viewData.data = data; //store employee data in the "viewData" object as "data"
+//     }).catch(() => {
+//         viewData.data = null; // set employee to null if there was an error
+//     }).then(data_service.getDepartments).then((data) => {
+//         viewData.departments = data; // store department data in the "viewData" object as "departments"
+//                                      // loop through viewData.departments and once we have found the departmentId that matches
+//                                      // the employee's "department" value, add a "selected" property to the matching
+//                                      // viewData.departments object
+//         for (let i = 0; i < viewData.departments.length; i++) {
+//             if (viewData.departments[i].departmentId == viewData.data[0].department) {
+//                 viewData.departments[i].selected = true;
+//             }
+//         }
+//         // if not add department set Selected to false and promto a message to user, message like "Please Choose Department" in html.
+//         if (viewData.departments[viewData.departments.length-1].departmentId != viewData.data[0].department) {
+//             viewData.departments.Selected = false;
+//         }
+//     }).catch(() => {
+//         viewData.departments = []; // set departments to empty if there was an error
+//     }).then(() => {
+//         if (viewData.data == null){ // if no employee - return an error
+//             res.status(404).send("Employee Not Found!!!");
+//         } else {
+//             res.render("employee", { viewData: viewData }); // render the "employee" view
+//         }
+//     });
+// });
 
-app.get("/managers", (req, res) => {
-    data_service.getManagers().then((data) => {
-        res.render("employeeList", { data: data, title: "Employees (Managers)" });
-    }).catch((err) => {
-        res.render("employeeList", { data: {}, title: "Employees (Managers)" });
-    });
-});
+// app.get("/managers", (req, res) => {
+//     data_service.getManagers().then((data) => {
+//         res.render("employeeList", { data: data, title: "Employees (Managers)" });
+//     }).catch((err) => {
+//         res.render("employeeList", { data: {}, title: "Employees (Managers)" });
+//     });
+// });
 
-app.get("/departments", (req, res) => {
-    data_service.getDepartments().then((data) => {
-        res.render("departmentList", { data: data, title: "Departments" });
-    }).catch((err) => {
-        res.render("departmentList", { data: {}, title: "Departments" });
-    });
-});
+// app.get("/departments", (req, res) => {
+//     data_service.getDepartments().then((data) => {
+//         res.render("departmentList", { data: data, title: "Departments" });
+//     }).catch((err) => {
+//         res.render("departmentList", { data: {}, title: "Departments" });
+//     });
+// });
 
-app.get("/employees/add", (req, res) => {
-    data_service.getDepartments().then((data) => {
-        res.render("addEmployee",{departments: data});
-    }).catch((err) => {
-        res.render("addEmployee", {departments: []});
-    });
-});
+// app.get("/employees/add", (req, res) => {
+//     data_service.getDepartments().then((data) => {
+//         res.render("addEmployee",{departments: data});
+//     }).catch((err) => {
+//         res.render("addEmployee", {departments: []});
+//     });
+// });
 
-app.get("/departments/add", (req, res) => {
-    res.render("addDepartment",{title: "Department"});
-});
+// app.get("/departments/add", (req, res) => {
+//     res.render("addDepartment",{title: "Department"});
+// });
 
-app.get("/employee/delete/:empNum", (req, res) => {
-    data_service.deleteEmployeeByNum(req.params.empNum).then((data) => {
-        res.redirect("/employees");
-    }).catch((err) => {
-        res.status(500).send("Unable to Remove Employee / Employee not found");
-    });
-});
+// app.get("/employee/delete/:empNum", (req, res) => {
+//     data_service.deleteEmployeeByNum(req.params.empNum).then((data) => {
+//         res.redirect("/employees");
+//     }).catch((err) => {
+//         res.status(500).send("Unable to Remove Employee / Employee not found");
+//     });
+// });
 
-app.get("/department/:departmentId", (req, res) => {
-    data_service.getDepartmentById(req.params.departmentId).then((data) => {
-        res.render("department", {
-           data: data
-        });
-    }).catch((err) => {
-        res.status(404).send("Department Not Found");
-    });
-});
+// app.get("/department/:departmentId", (req, res) => {
+//     data_service.getDepartmentById(req.params.departmentId).then((data) => {
+//         res.render("department", {
+//            data: data
+//         });
+//     }).catch((err) => {
+//         res.status(404).send("Department Not Found");
+//     });
+// });
 
-/////////////////////////////////////Post Route /////////////////////////////////
+// /////////////////////////////////////Post Route /////////////////////////////////
 
-app.post("/employees/add", (req, res) => {
-    data_service.addEmployee(req.body).then((data) => {
-        res.redirect("/employees");
-    }).catch((err) => {
-        console.log(err);
-    });
-});
+// app.post("/employees/add", (req, res) => {
+//     data_service.addEmployee(req.body).then((data) => {
+//         res.redirect("/employees");
+//     }).catch((err) => {
+//         console.log(err);
+//     });
+// });
 
-app.post("/employees/update", (req, res) => {
-    res.redirect("/employees");
-});
+// app.post("/employees/update", (req, res) => {
+//     res.redirect("/employees");
+// });
 
-app.post("/employee/update", (req, res) => {
-    data_service.updateEmployee(req.body).then((data) => {
-        res.redirect("/employees");
-    }).catch((err) => {
-        console.log(err);
-    });
-});
+// app.post("/employee/update", (req, res) => {
+//     data_service.updateEmployee(req.body).then((data) => {
+//         res.redirect("/employees");
+//     }).catch((err) => {
+//         console.log(err);
+//     });
+// });
 
-app.post("/departments/add", (req, res) => {
-    data_service.addDepartment(req.body).then((data) => {
-        res.redirect("/departments");
-    }).catch(() => {
-        console.log(err);
-    });
-});
+// app.post("/departments/add", (req, res) => {
+//     data_service.addDepartment(req.body).then((data) => {
+//         res.redirect("/departments");
+//     }).catch(() => {
+//         console.log(err);
+//     });
+// });
 
-app.post("/department/update", (req,res) => {
-    data_service.updateDepartment(req.body).then((data) => {
-        res.redirect("/departments");
-    });
-});
+// app.post("/department/update", (req,res) => {
+//     data_service.updateDepartment(req.body).then((data) => {
+//         res.redirect("/departments");
+//     });
+// });
 
-app.use((req, res) => {
-    res.status(404).send("Sorry!!!!!!!>>>Page Not Found! <<<:(");
-});
+// app.use((req, res) => {
+//     res.status(404).send("Sorry!!!!!!!>>>Page Not Found! <<<:(");
+// });
 
-app.listen(HTTP_PORT, onHttpStart);
+// app.listen(HTTP_PORT, onHttpStart);
